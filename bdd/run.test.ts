efcaad6f08f4bdbc499243a463d47d844c5b6fb0
sean <sean@deepracticex.com>
2026-03-05 08:37:15 +0800

    fix: update @deepracticex/bdd dependency to version 0.3.0 and refactor test configuration

--- bdd/run.test.ts
+++ bdd/run.test.ts
@@ -1,10 +1,6 @@
-import "./support/world";
-import "./steps/issue.steps";
-import "./steps/comment.steps";
-import "./steps/label.steps";
-import { loadFeature } from "@deepracticex/bdd";
+import { configure } from "@deepracticex/bdd";
 
-loadFeature("features/issue-lifecycle.feature");
-loadFeature("features/issue-filtering.feature");
-loadFeature("features/comments.feature");
-loadFeature("features/labels.feature");
+await configure({
+  features: "features/**/*.feature",
+  steps: ["support/**/*.ts", "steps/**/*.ts"],
+});
